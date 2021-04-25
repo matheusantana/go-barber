018763ac7cb7112ae9e7e2238b37a06af8df2f0a
Matheus Santana <matheusoliveirab13@gmail.com>
2021-04-25 18:38:01 -0300

    continuação

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'omatheusantana@teste.com',
+      name: 'Matheus Santana',
+    },
+  },
+} as IMailConfig;
